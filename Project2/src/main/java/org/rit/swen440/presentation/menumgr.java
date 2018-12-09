aa0693b129b6f03b369eb509d1a93cd1de27406b
Matt Newell <developer.newell@gmail.com>
2018-12-08 22:41:20 -0500

    Fix NPE caused by session purchase history

--- Project2/src/main/java/org/rit/swen440/presentation/menumgr.java
+++ Project2/src/main/java/org/rit/swen440/presentation/menumgr.java
@@ -6,8 +6,7 @@ import org.rit.swen440.dataLayer.Category;
 import java.util.*;
 
 
-public class menumgr
-{
+public class menumgr {
     int currentLevel = 0;
     String currentCategoryName;
     String currentItemName;
@@ -16,16 +15,13 @@ public class menumgr
     private Controller controller;
     private HashMap<String, Integer> sessionPurchases = new HashMap<String, Integer>();
 
-    public menumgr()
-    {
+    public menumgr() {
         controller = new Controller(System.getProperty("fileSystemRoot"));
     }
 
-    public boolean loadLevel(int level)
-    {
+    public boolean loadLevel(int level) {
 //        System.out.println("Loading level:" + currentLevel);
-        switch (currentLevel)
-        {
+        switch (currentLevel) {
             case -1:
                 return true;
             case 0:
@@ -47,43 +43,32 @@ public class menumgr
         return false;
     }
 
-    public void Level0()
-    {
+    public void Level0() {
         menu m = new menu();
         List<String> categories = controller.getCategories();
         m.loadMenu(categories);
-        m.addMenuItem("'q' to Quit"); 
+        m.addMenuItem("'q' to Quit");
         m.addMenuItem("'p' to view Session Purchases");
         System.out.println("The following org.rit.swen440.presentation.categories are available");
         m.printMenu();
         String result = "0";
-        try
-        {
+        try {
             result = m.getSelection();
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             result = "q";
         }
-        if (Objects.equals(result,"q"))
-        {
+        if (Objects.equals(result, "q")) {
             currentLevel--;
-        }
-        else if (Objects.equals(result, "p"))
-        {
-        	System.out.println("You have purchased:");
-        	for (String product: sessionPurchases.keySet()) 
-        	{
-        		System.out.println(" - " + sessionPurchases.get(product) + " " + product + "(s)");
-        	}
-        	if (sessionPurchases.keySet().size() == 0) 
-        	{
-        		System.out.println(" - NOTHING. BUY STUFF");
-        	}
-        	System.out.println("");
-        }
-        else
-        {
+        } else if (Objects.equals(result, "p")) {
+            System.out.println("You have purchased:");
+            for (String product : sessionPurchases.keySet()) {
+                System.out.println(" - " + sessionPurchases.get(product) + " " + product + "(s)");
+            }
+            if (sessionPurchases.keySet().size() == 0) {
+                System.out.println(" - NOTHING. BUY STUFF");
+            }
+            System.out.println("");
+        } else {
             currentLevel++;
             int iSel = Integer.parseInt(result);
 
@@ -92,8 +77,7 @@ public class menumgr
         }
     }
 
-    public void Level1()
-    {
+    public void Level1() {
         menu m = new menu();
 
         //items it = new items("orderSys/" + currentCategory.getName());
@@ -102,57 +86,47 @@ public class menumgr
         List<String> itemList = controller.getProducts(currentCategoryName);
         List<String> l = new ArrayList<>();
         System.out.println("");
-        for (String itm: itemList)
+        for (String itm : itemList)
             l.add(controller.getProductInformation(currentCategoryName, itm, Controller.PRODUCT_FIELD.NAME)
-             + "($" + controller.getProductInformation(currentCategoryName, itm, Controller.PRODUCT_FIELD.COST) + ")");
-        
+                    + "($" + controller.getProductInformation(currentCategoryName, itm, Controller.PRODUCT_FIELD.COST) + ")");
+
         m.loadMenu(l);
         m.addMenuItem("'q' to quit");
         System.out.println("The following items are available");
         m.printMenu();
         String result = m.getSelection();
-        try
-        {
+        try {
             int iSel = Integer.parseInt(result);//Item  selected
             currentItemName = itemList.get(iSel);
             //currentItem = itemList.get(iSel);
             //Now read the file and print the org.rit.swen440.presentation.items in the catalog
             System.out.println("You want item from the catalog: " + currentItemName);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             result = "q";
         }
         if (Objects.equals(result, "q"))
             currentLevel--;
-        else if (Objects.equals(result, "p"))
-        {
-        	System.out.println("You have purchased:");
-        	for (String product: sessionPurchases.keySet()) 
-        	{
-        		System.out.println(" - " + sessionPurchases.get(product) + " " + product + "(s)");
-        	}
-        	if (sessionPurchases.keySet().size() == 0) 
-        	{
-        		System.out.println(" - NOTHING. BUY STUFF");
-        	}
-        	System.out.println("");
-        }
-        else
-        {
+        else if (Objects.equals(result, "p")) {
+            System.out.println("You have purchased:");
+            for (String product : sessionPurchases.keySet()) {
+                System.out.println(" - " + sessionPurchases.get(product) + " " + product + "(s)");
+            }
+            if (sessionPurchases.keySet().size() == 0) {
+                System.out.println(" - NOTHING. BUY STUFF");
+            }
+            System.out.println("");
+        } else {
             //currentLevel++;//Or keep at same level?
             OrderQty(currentCategoryName, currentItemName);
         }
     }
 
 
-    public void Level2()
-    {
-    	
+    public void Level2() {
+
     }
 
-    public void OrderQty(String category, String item)
-    {
+    public void OrderQty(String category, String item) {
         System.out.println("Please select a quantity");
         System.out.println(controller.getProductInformation(category, item, Controller.PRODUCT_FIELD.NAME) +
                 " availability:" + controller.getProductInformation(category, item, Controller.PRODUCT_FIELD.INVENTORY));
@@ -160,11 +134,11 @@ public class menumgr
         menu m = new menu();
         String result = m.getSelection();
         System.out.println("You ordered:" + result);
-		if (sessionPurchases.containsKey(item)) {
-			sessionPurchases.put(item, Integer.parseInt(result)); 
-		}
-        else {
-			sessionPurchases.put(item, sessionPurchases.get(item) + Integer.parseInt(result));
-		}      
+        if (sessionPurchases.containsKey(item)) {
+            sessionPurchases.put(item, Integer.parseInt(result));
+        } else {
+            sessionPurchases.put(item, (sessionPurchases.get(item) == null ? 0 : sessionPurchases.get(item))
+                    + Integer.parseInt(result));
+        }
     }
 }
